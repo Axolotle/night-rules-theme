57189fe2739474e88308644553b27ed36854ffdb
Axolotle <nchesnais@mailoo.org>
2019-02-08 01:22:39 +0100

    remove js and add url anchor to main element in pages

--- js/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var elem = document.querySelector('h2');
-    if (elem.scrollIntoView === undefined) {
-        window.scrollTo(0, document.getElementsByTagName("NAV")[0].offsetHeight);
-        console.log('oppppp');
-    } else {
-        elem.scrollIntoView({behavior: "instant", block: "center"});
-    }
-}
